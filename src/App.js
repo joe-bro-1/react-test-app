d760b37d6e411f77060f4f63634b2afb3782f8c2
joe-bro-1 <yosefbrody@icloud.com>
2022-12-21 08:47:14 -0500

    new link

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="http://gametest.mobi/pixi/morph"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          check em out
         </a>
       </header>
     </div>
